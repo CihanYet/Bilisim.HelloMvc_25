d66a24e29546a82fdc5371fd2ef6cd590ccde6bf
Gazi <Gazi@DESKTOP-224SE1B>
2025-03-11 14:06:16 +0300

    Add project files.

---
+++ Bilisim.HelloMvc/Controllers/OgrenciController.cs
@@ -0,0 +1,38 @@
+﻿using Bilisim.HelloMvc.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bilisim.HelloMvc.Controllers
+{
+    public class OgrenciController : Controller
+    {
+        public ViewResult Index()//Action
+        {
+            return View("AnaSayfa");
+        }
+        public ViewResult OgrenciDetay(int id)
+        {
+
+            Ogrenci ogrenci = null;
+
+            if (id == 1)
+            {
+                ogrenci = new Ogrenci { Ogrenciid = 1, Ad = "Ali", Soyad = "Veli" };
+            }
+            else if (id == 2)
+            {
+                ogrenci = new Ogrenci { Ogrenciid = 2, Ad = "Ahmet", Soyad = "Mehmet" };
+                
+            }
+            
+            ViewData["ogr"] = ogrenci;
+
+            return View();
+        }
+    }
+}
+
+//Controllerdan->View'e veri taşıma
+//1-ViewData: Koleksiyondur.Anahtar-Değer çiftinden oluşur. (Key-Value Pair)
+// Keyler tekil olmalıdır. Keyler string, Value'lar object'dir.
+//2-ViewBag
+//3-Model
