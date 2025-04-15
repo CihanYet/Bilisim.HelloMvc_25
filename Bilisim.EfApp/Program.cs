46509ba09911b15d55207e50ba5ed1726c8128eb
Gazi <Gazi@DESKTOP-224SE1B>
2025-04-15 15:54:46 +0300

    HTML Helpers,TagHelpers,EF Code First

---
+++ Bilisim.EfApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Bilisim.EfApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
